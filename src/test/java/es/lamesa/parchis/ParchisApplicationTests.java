5d0e39b5a7062fa9dae753f219544b20a031ef8e
Diego Domingo <97918691+DiegoDomingo@users.noreply.github.com>
2023-03-06 16:32:36 +0100

    Add files via upload

---
+++ src/test/java/es/lamesa/parchis/ParchisApplicationTests.java
@@ -0,0 +1,13 @@
+package es.lamesa.parchis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParchisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
