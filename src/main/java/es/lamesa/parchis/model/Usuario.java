049ef305816229a69ca3704e2d36d9cec0e34b7a
diegodom02 <818637@unizar.es>
2023-03-10 10:43:54 +0100

    Usuario class
    Co-authored-by: Pablo Cervera <815538@unizar.es>
    Co-authored-by: Sergio Berges Aguaviva <816380@unizar.es>

---
+++ src/main/java/es/lamesa/parchis/model/Usuario.java
@@ -0,0 +1,64 @@
+package es.lamesa.parchis.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "int default 0")
+    private int numMonedas;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getNumMonedas() {
+        return this.numMonedas;
+    }
+
+    public void setNumMonedas(int numMonedas) {
+        this.numMonedas = numMonedas;
+    }
+}
