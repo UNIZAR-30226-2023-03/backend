48b0f69c0f8049c8c42c1de390a4fef750e0e8d7
diegodom02 <818637@unizar.es>
2023-03-11 12:03:00 +0100

    Cambios seguridad
    Co-authored-by: Pablo Cervera <815538@unizar.es>
    Co-authored-by: Sergio Berges Aguaviva <816380@unizar.es>

--- src/main/java/es/lamesa/parchis/ParchisApplication.java
+++ src/main/java/es/lamesa/parchis/ParchisApplication.java
@@ -2,27 +2,12 @@ package es.lamesa.parchis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParchisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParchisApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("*")
-						.allowedMethods("GET", "POST");
-			}
-        };
 	}
 
 }
--- src/main/java/es/lamesa/parchis/WebMvcConfig.java
+++ src/main/java/es/lamesa/parchis/WebMvcConfig.java
@@ -11,10 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**");
     }
+    
 }
