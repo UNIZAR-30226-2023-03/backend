d86562803f40601a16c01211b034cfbb6b4af6d2
diegodom02 <818637@unizar.es>
2023-03-11 13:58:33 +0100

    Prueba

--- src/main/java/es/lamesa/parchis/WebMvcConfig.java
+++ src/main/java/es/lamesa/parchis/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://frontend-web-9sco.onrender.com")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*");
     }
--- src/main/java/es/lamesa/parchis/repository/UsuarioRepository.java
+++ src/main/java/es/lamesa/parchis/repository/UsuarioRepository.java
@@ -3,11 +3,9 @@ package es.lamesa.parchis.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import es.lamesa.parchis.model.Usuario;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     @Query("SELECT u FROM Usuario u WHERE u.username = :login OR u.email = :login")
