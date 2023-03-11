3f3244da6af5537fbd0376045297aed4293d1023
PabloCervera <815538@unizar.es>
2023-03-11 16:15:37 +0100

    Clase Dado

---
+++ src/main/java/es/lamesa/parchis/model/dto/Dado.java
@@ -0,0 +1,14 @@
+package es.lamesa.parchis.model.dto;
+
+public class Dado {
+    private int num;
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+}
