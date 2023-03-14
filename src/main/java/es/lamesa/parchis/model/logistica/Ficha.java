25514256bddaf075651631ebcebc2ad40fff8187
PabloCervera <815538@unizar.es>
2023-03-14 16:11:10 +0100

    Actualización clase ficha

--- src/main/java/es/lamesa/parchis/model/logistica/Ficha.java
+++ src/main/java/es/lamesa/parchis/model/logistica/Ficha.java
@@ -2,6 +2,8 @@ package es.lamesa.parchis.model.logistica;
 
 public class Ficha {
     private Color color;
+
+    private int casilla;
     //¿id ó numFicha para ficha para distinguirla de las otras fichas del mismo color?
     //P.e id = ROJO_1, ROJO_2, etc.
     //se les podría asignar un id según el orden en el que salgan de casa.
