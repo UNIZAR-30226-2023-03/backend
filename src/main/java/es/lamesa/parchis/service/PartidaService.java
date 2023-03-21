9171040ebf262f4313c50e52a52fd45418dd9e89
PabloCervera <815538@unizar.es>
2023-03-21 17:33:42 +0100

    Quitar transactional

--- src/main/java/es/lamesa/parchis/service/PartidaService.java
+++ src/main/java/es/lamesa/parchis/service/PartidaService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 
 import es.lamesa.parchis.repository.PartidaRepository;
 import es.lamesa.parchis.repository.UsuarioPartidaRepository;
@@ -32,7 +31,6 @@ public class PartidaService {
         return pRepository.findAll();
     }
     
-    @Transactional
     public Partida crearPartida(PartidaDto partidaDto) {
         if (pRepository.findByNombreAndEstado(partidaDto.getNombre()) == null) {
             Partida partida = new Partida();
