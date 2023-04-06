71c4c36ddd3a83a8e620c51910c9cae8056c70d9
diegodom02 <818637@unizar.es>
2023-04-06 11:52:25 +0200

    Prueba

--- src/main/java/es/lamesa/parchis/config/WebSocket.java
+++ src/main/java/es/lamesa/parchis/config/WebSocket.java
@@ -13,7 +13,7 @@ public class WebSocket implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://212.166.220.223:3000").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://192.168.121.86:3000").withSockJS();
     }
 
     @Override
