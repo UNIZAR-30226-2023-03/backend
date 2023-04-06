9da28a23a8a05e72e50d796a332876258401bca6
diegodom02 <818637@unizar.es>
2023-04-06 11:42:03 +0200

    Prueba

--- src/main/java/es/lamesa/parchis/config/WebConfig.java
+++ src/main/java/es/lamesa/parchis/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://212.166.220.223:3000");
+        registry.addMapping("/**");
     }
 
 }
